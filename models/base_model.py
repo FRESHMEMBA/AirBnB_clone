bc73e355806ff57d6028a4492af337c023c07ccc
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-02 22:30:07 +0200

    4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -46,16 +46,12 @@ class BaseModel:
         if kwargs:
             self.id = kwargs.get("id", str(uuid.uuid4()))
             self.created_at = datetime.strptime(
-                kwargs.get(
-                    "created_at",
-                    datetime.now().isoformat()),
-                    "%y-%m-%dT%H:%M:%S.%f"
+                kwargs.get("created_at", datetime.now()),
+                "%y-%m-%dT%H:%M:%S.%f"
                 )
             self.updated_at = datetime.strptime(
-                kwargs.get(
-                    "updated_at",
-                    datetime.now().isoformat()),
-                    "%y-%m-%dT%H:%M:%S.%f"
+                kwargs.get("updated_at", datetime.now()),
+                "%y-%m-%dT%H:%M:%S.%f"
                 )
         else:
             self.id = str(uuid.uuid4())
