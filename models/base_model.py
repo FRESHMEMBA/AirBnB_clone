ee6d4dc2b61e395f04616e15ed40b961051e5a2f
FRESHMEMBA <mazibukosibonelo@gmail.com>
2024-04-03 01:33:14 +0200

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()
+            storage.new(self)
 
     def __str__(self) -> str:
         """
